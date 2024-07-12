90806e876d94e1ee07955ebe3ee651f463fbc535
envyafish <orekiii234@gmail.com>
2024-07-12 20:51:50 +0800

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -25,89 +25,65 @@ export default defineConfig({
                 ],
             },
             registerType: 'autoUpdate',
-            workbox: VITE_MODE === 'production'
-                ? {
-                    globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg}'], //缓存相关静态资源
-                    runtimeCaching: [
-                        // 配置自定义运行时缓存
-                        VITE_MODE !== 'production'
-                            ? {
-                                urlPattern: ({url}) =>
-                                    url.origin === 'https://app-api-0.com',
-                                handler: 'NetworkFirst',
-                                options: {
-                                    cacheName: 'wisbayar-api',
-                                    cacheableResponse: {
-                                        statuses: [200],
-                                    },
-                                },
-                            }
-                            : {
-                                urlPattern: ({url}) =>
-                                    url.origin === 'https://app-api.id',
-                                handler: 'NetworkFirst',
-                                options: {
-                                    cacheName: 'wisbayar-api',
-                                    cacheableResponse: {
-                                        statuses: [200],
-                                    },
-                                },
-                            },
-                        {
-                            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-                            handler: 'CacheFirst',
-                            options: {
-                                cacheName: 'wisbayar-images',
-                                expiration: {
-                                    // 最多30个图
-                                    maxEntries: 30,
-                                },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg}'], //缓存相关静态资源
+                runtimeCaching: [
+                    // 配置自定义运行时缓存
+                    {
+                        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'wisbayar-images',
+                            expiration: {
+                                // 最多30个图
+                                maxEntries: 30,
                             },
                         },
-                        {
-                            urlPattern: /.*\.js.*/,
-                            handler: 'StaleWhileRevalidate',
-                            options: {
-                                cacheName: 'wisbayar-js',
-                                expiration: {
-                                    maxEntries: 30, // 最多缓存30个，超过的按照LRU原则删除
-                                    maxAgeSeconds: 30 * 24 * 60 * 60,
-                                },
-                                cacheableResponse: {
-                                    statuses: [200],
-                                },
+                    },
+                    {
+                        urlPattern: /.*\.js.*/,
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'wisbayar-js',
+                            expiration: {
+                                maxEntries: 30, // 最多缓存30个，超过的按照LRU原则删除
+                                maxAgeSeconds: 30 * 24 * 60 * 60,
+                            },
+                            cacheableResponse: {
+                                statuses: [200],
                             },
                         },
-                        {
-                            urlPattern: /.*\.css.*/,
-                            handler: 'StaleWhileRevalidate',
-                            options: {
-                                cacheName: 'wisbayar-css',
-                                expiration: {
-                                    maxEntries: 20,
-                                    maxAgeSeconds: 30 * 24 * 60 * 60,
-                                },
-                                cacheableResponse: {
-                                    statuses: [200],
-                                },
+                    },
+                    {
+                        urlPattern: /.*\.css.*/,
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'wisbayar-css',
+                            expiration: {
+                                maxEntries: 20,
+                                maxAgeSeconds: 30 * 24 * 60 * 60,
+                            },
+                            cacheableResponse: {
+                                statuses: [200],
                             },
                         },
-                        {
-                            urlPattern: /.*\.html.*/,
-                            handler: 'StaleWhileRevalidate',
-                            options: {
-                                cacheName: 'wisbayar-html',
-                                expiration: {
-                                    maxEntries: 20,
-                                    maxAgeSeconds: 30 * 24 * 60 * 60,
-                                },
-                                cacheableResponse: {
-                                    statuses: [200],
-                                },
+                    },
+                    {
+                        urlPattern: /.*\.html.*/,
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'wisbayar-html',
+                            expiration: {
+                                maxEntries: 20,
+                                maxAgeSeconds: 30 * 24 * 60 * 60,
+                            },
+                            cacheableResponse: {
+                                statuses: [200],
                             },
                         },
-                    ],
-                } : {},
+                    },
+                ],
+            },
             devOptions: {
                 enabled: true,
             }
